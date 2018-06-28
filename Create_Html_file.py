c7db845420db5d15c6088f7e10615abb9c6fcd3d
glennsvel90 <glennsvelu@gmail.com>
2018-06-27 22:46:38 -0400

    create html

--- Create_Html_file.py
+++ Create_Html_file.py
@@ -2,19 +2,26 @@ from datetime import datetime
 from Get_Weather_Data import get_weather_data
 
 def create_html_report(data_dict, icon_url, html_file):
+
     alt_var = data_dict['weather']
 
     with open(html_file, mode='w') as outfile:
-        outfile.write('\t<tr><td align="center">' + datetime.now().strftime(
-        "%m-%d-%Y %H:%M:%S") + '</td></tr><br>\n')
-        outfile.write('<img alt={} src=')
+        outfile.write( '\t<tr><td align="center">' + datetime.now()
+                       .strftime("%Y-%m-%d %H:%M:%S") +'</td></tr><br>\n' )
+        outfile.write( '<img alt={} src={}>'.format(alt_var, icon_url))
+        outfile.write( '<br><span style="color:blue"><b>\tWeather Data:</b>\n' )
+        outfile.write( '<br>')
 
+        outfile.write( '<html><table border=1>\n' )
+        #--------------------------------------------
+        for key, value in data_dict.items():
+            outfile.write( '<tr><td><b><span style="color:black">{:s}</b></td><td align="left"> \
+                <span style="color:blue"><b>{:s}</b></td></tr>\n'.format(key, value))
+        #--------------------------------------------
+        outfile.write( '</table></html>\n' )
 
-
-
-
-
-
+#===========================================
 if __name__ == '__main__':
-    weather_dict, icon = get_weather_data('KNYC')
+
+    weather_dict, icon = get_weather_data('KLAX')
     create_html_report(weather_dict, icon, "Test_Email_File.html")
--- Get_Weather_Data.py
+++ Get_Weather_Data.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from pprint import pprint
 
 
+#Retrieve the tags we are interested in
 weather_data_tags_dict = {
     'observation_time': '',
     'weather': '',
@@ -18,26 +19,31 @@ weather_data_tags_dict = {
     'location': ''
     }
 
-# ---------------
+# ---------------------------------------------------------------
 
-
-def get_weather_data(station_id='KNYC'):
+def get_weather_data(station_id='KLAX'):
     url_general = 'http://www.weather.gov/xml/current_obs/{}.xml'
     url = url_general.format(station_id)
-    request = urllib.request.urlopen(url)
+    request = urllib.request.urlopen( url )
     content = request.read().decode()
 
+    # Using ElementTree to retrieve specific tags from the xml
+
     xml_root = ET.fromstring(content)
+
     for data_point in weather_data_tags_dict.keys():
         weather_data_tags_dict[data_point] = xml_root.find(data_point).text
 
-    icon_url_base = xml_root.find('icon_url_base').text
-    icon_url_name = xml_root.find('icon_url_name').text
+    icon_url_base = xml_root.find('icon_url_base' ).text
+    icon_url_name = xml_root.find('icon_url_name' ).text
     icon_url = icon_url_base + icon_url_name
 
     return weather_data_tags_dict, icon_url
 
-if __name__== '__main__':
+
+#===========================================
+if __name__ == '__main__':
     weather_dict, icon = get_weather_data()
+
     pprint(weather_dict)
     print(icon)
